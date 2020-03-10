dbe1b82357a5fb5ce353bd6095d17a84cdbef138
mkaspryk <mkaspryk09@outlook.com>
2020-03-10 23:00:55 +0100

    added-StartPage-LoginPage-RegisterPage

--- stock/home/views.py
+++ stock/home/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'index.html', {})
 
+def register(request):
+	return render(request, 'registerPanel.html',{})
+
+def login(request):
+	return render(request, 'loginPanel.html',{})
 
--- stock/stock/settings.py
+++ stock/stock/settings.py
@@ -4,7 +4,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -108,3 +107,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ('stock/home/static',)
