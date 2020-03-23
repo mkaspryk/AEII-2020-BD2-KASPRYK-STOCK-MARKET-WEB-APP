dc3e4e3db541b394acd936e252aee193e1e4c59f
Maciej Król <maciej.krol@itsilesia.com>
2020-03-23 17:10:44 +0100

    Ready apiHandler

--- stock/crypto_stock/ApiHandler.py
+++ stock/crypto_stock/ApiHandler.py
@@ -2,8 +2,6 @@ import requests
 import json
 from requests.models import PreparedRequest
 
-
-
 class ApiHandler:
     def __init__(self):
         self.api_key = '91f2304795012ff76d8b626a23279468c28a6d8139b9ca07a9e9fcddb323c84d'
@@ -15,15 +13,21 @@ class ApiHandler:
         self.currency_symbol_param = 'tsym'
         self.data_limit_param = 'limit'
 
+    def get_history(self,crypto_symbol,currency_symbol,data_url,data_limit='10') :
+        params = { self.crypto_symbol_param : crypto_symbol, self.currency_symbol_param : currency_symbol, self.data_limit_param : data_limit }
+        req = PreparedRequest()
+        req.prepare_url(data_url,params)
+        daily_history_data = requests.get(req.url)
+        json_text = json.loads(daily_history_data.content)
+        json_data = json_text['Data']['Data']
+        return json_data
 
     def get_daily_history(self,crypto_symbol,currency_symbol,data_limit='10'):
-        params = { self.crypto_symbol_param : crypto_symbol, self.currency_symbol_param : crypto_symbol, self.data_limit_param : data_limit }
-        req = PreparedRequest()
-        req.prepare_url(self.daily_historical_data_url,params)
-        return req.url
+        return self.get_history(crypto_symbol,currency_symbol,self.daily_historical_data_url,data_limit)
 
     def get_hourly_history(self,crypto_symbol,currency_symbol,data_limit='10'):
-        pass
+        return self.get_history(crypto_symbol,currency_symbol,self.hourly_historical_data_url,data_limit)
+
 
     def get_minute_history(self,crypto_symbol,currency_symbol,data_limit='10'):
-        pass
+        return self.get_history(crypto_symbol,currency_symbol,self.minute_historical_data_url,data_limit)
--- stock/crypto_stock/views.py
+++ stock/crypto_stock/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from . import ApiHandler
+import json
 
 
 def crypto_stock(request):
 	api_handler = ApiHandler.ApiHandler()
-	api_handler.get_daily_history('BTC','USD')
-	return render(request, 'crypto_stock.html', {})
+	daily_history_data = api_handler.get_daily_history('BTC','USD')
+	return render(request, 'crypto_stock.html', {'daily_history_data': daily_history_data})
