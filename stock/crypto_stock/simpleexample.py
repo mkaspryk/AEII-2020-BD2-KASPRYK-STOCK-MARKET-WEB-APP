11b5432a7ca0b6c5286d8f465cf16928581dbf93
Maciej Król <maciej.krol@itsilesia.com>
2020-04-04 23:43:18 +0200

    Fix for previous commit

--- stock/crypto_stock/simpleexample.py
+++ stock/crypto_stock/simpleexample.py
@@ -8,20 +8,21 @@ from datetime import datetime
 
 x_data = []
 y_data_scatter = []
-y_data_box = []
+y_data_box_open = []
+y_data_box_close = []
+y_data_box_low = []
+y_data_box_high = []
 
 api_handler = api_handling.ApiHandler()
-data = api_handler.get_minute_history('BTC', 'USD')
+data = api_handler.get_minute_history('BTC', 'USD', 200)
 
 for dictionary in data:
     # average = str((float(dictionary['open']) + float(dictionary['close']) + float(dictionary['low']) + float(dictionary['high']))/ 4)
     x_data.append(datetime.fromtimestamp(int(dictionary['time'])))
-    y_data_box.append({
-                'open' : dictionary['open'],
-                'close' : dictionary['close'],
-                'low' : dictionary['low'],
-                'high': dictionary['high']
-    })
+    y_data_box_open.append(float(dictionary['open']))
+    y_data_box_close.append(float(dictionary['close']))
+    y_data_box_low.append(float(dictionary['low']))
+    y_data_box_high.append(float(dictionary['high']))
     y_data_scatter.append(dictionary['open'])
 
 
@@ -47,7 +48,7 @@ app.layout = html.Div([
               [Input('slider-updatemode', 'value')])
 def display_value(value):
     global x_data
-    global y_data_box
+    global y_data_box_open
     graphs = []
 
     print(y_data_scatter[0])
@@ -60,17 +61,17 @@ def display_value(value):
 
     graphs.append(go.Ohlc(
         x = x_data,
-        open = y_data_box['open'],
-        high = y_data_box['high'],
-        low = y_data_box['low'],
-        close = y_data_box['close']
+        open = y_data_box_open,
+        high = y_data_box_high,
+        low = y_data_box_low,
+        close = y_data_box_close
     ))
 
     layout = go.Layout(
         paper_bgcolor='#27293d',
         plot_bgcolor='rgba(0,0,0,0)',
         xaxis=dict(range=[min(x_data), max(x_data)]),
-        yaxis=dict(range=[min(y_data_box), max(y_data_box)]),
+        yaxis=dict(range=[min(y_data_box_open), max(y_data_box_open)]),
         font=dict(color='white'),
 
     )
