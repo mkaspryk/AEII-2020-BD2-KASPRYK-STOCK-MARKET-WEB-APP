6734b0553185677fda268f62bfc9927ecfeb677e
Maciej Król <maciej.krol@itsilesia.com>
2020-04-09 02:43:34 +0200

    add radio buttons for changing graph mode TODO imporve visual site

--- stock/crypto_stock/graphs.py
+++ stock/crypto_stock/graphs.py
@@ -13,15 +13,36 @@ y_data_box_close = []
 y_data_box_low = []
 y_data_box_high = []
 
-def return_app(crypto_symbol = None):
+app = None
+
+colors = {
+    'background': '#27293d',
+    'text': '#ffffff'
+}
+
+history_precision = ['minute','hour','day']
+
+def refresh_data(crypto_symbol, history_precision):
+
     data = None
     api_handler = api_handling.ApiHandler()
-    if(crypto_symbol == '' or crypto_symbol is None):
-        data = api_handler.get_hourly_history('BTC', 'USD', 200)
-    else:
-        data = api_handler.get_hourly_history(crypto_symbol, 'USD', 200)
+    try:
+        if history_precision == 'minute':
+            data = api_handler.get_minute_history(crypto_symbol, 'USD', 200)
+        elif history_precision == 'hour':
+            data = api_handler.get_hourly_history(crypto_symbol, 'USD', 200)
+        elif history_precision == 'day':
+            data = api_handler.get_daily_history(crypto_symbol, 'USD', 100)
+    except KeyError:
+        # TODO handling KeyError from python form
+        return
+    x_data.clear()
+    y_data_box_open.clear()
+    y_data_scatter.clear()
+    y_data_box_close.clear()
+    y_data_box_low.clear()
+    y_data_box_high.clear()
     for dictionary in data:
-        # average = str((float(dictionary['open']) + float(dictionary['close']) + float(dictionary['low']) + float(dictionary['high']))/ 4)
         x_data.append(datetime.fromtimestamp(int(dictionary['time'])))
         y_data_box_open.append(float(dictionary['open']))
         y_data_box_close.append(float(dictionary['close']))
@@ -30,32 +51,54 @@ def return_app(crypto_symbol = None):
         y_data_scatter.append(dictionary['open'])
 
 
+def return_app(crypto_symbol = None):
+    global app
+    if(crypto_symbol == '' or crypto_symbol is None):
+        crypto_symbol = 'BTC'
+    refresh_data(crypto_symbol, 'hour')
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     app = DjangoDash('SimpleExample', external_stylesheets=external_stylesheets)
 
+    app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+        html.H1(children=crypto_symbol +' history',
+                style={
+                    'padding': '20px',
+                    'textAlign': 'left',
+                    'color': colors['text']
+                }
+        ),
 
-    app.layout = html.Div([
-        html.H1('Square Root Slider Graph'),
-        dcc.Graph(id='slider-graph', animate=True, style={"backgroundColor": "#1a2d46", 'color': '#ffffff'}),
-        dcc.Slider(
-            id='slider-updatemode',
-            marks={i: '{}'.format(i) for i in range(20)},
-            max=20,
-            value=2,
-            step=1,
-            updatemode='drag',
+        dcc.Graph(
+            id='slider-graph',
+            animate=True,
+            style={"backgroundColor": "#1a2d46", 'color': '#ffffff'},
+            figure={
+                'data': [],
+                'layout': {
+                    'plot_bgcolor': colors['background'],
+                    'paper_bgcolor': colors['background'],
+                    'font': {
+                        'color': colors['text']
+                    }
+                }
+            }
+        ),
+
+        dcc.RadioItems(
+            id='mode-radio',
+            options=[{'label': value, 'value': value} for value in history_precision],
+            value='hour',
+            style={"color" : colors['text'], "padding" : "20px"}
         ),
     ])
 
-
     @app.callback(Output('slider-graph', 'figure'),
-                  [Input('slider-updatemode', 'value')])
-    def display_value(value):
+                  [Input('mode-radio', 'value')])
+    def change_mode(value):
+        refresh_data(crypto_symbol, value)
         global x_data
         global y_data_box_open
         graphs = []
-
-        print(y_data_scatter[0])
 
         graphs.append(go.Scatter(
             x=x_data,
@@ -77,6 +120,5 @@ def return_app(crypto_symbol = None):
             xaxis=dict(range=[min(x_data), max(x_data)]),
             yaxis=dict(range=[min(y_data_box_open), max(y_data_box_open)]),
             font=dict(color='white'),
-
         )
         return {'data': graphs, 'layout': layout}
