f5adcc8da2723c2057ebd40e2d78e21f7ac15f3c
Maciej Król <maciej.krol16@interia.pl>
2020-07-29 16:30:02 +0200

    prepared backend for guilds

--- stock/crypto_guilds/models.py
+++ stock/crypto_guilds/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Guild(models.Model):
-    guild_name = models.CharField(max_length=50)
-    guild_master = models.CharField(max_length=50)
-    guild_members = models.TextField()
-    guild_score = models.IntegerField()
-    guild_money = models.FloatField()
+    name = models.CharField(max_length=50, required = True)
+    master = models.OneToOneField(GuildMember)
+    members = model.ForeignKey(GuildMember)
+    max_members = models.IntegerField(min_value=1, max_value=50, required = True)
+    score = models.FloatField()
+    money = models.DecimalField(max_digits=20,decimal_places=2)
 
     def __str__():
-        return self.guild_name
+        return self.name
+
+class GuildMember(models.Model)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild)
--- stock/guild_creation/views.py
+++ stock/guild_creation/views.py
@@ -6,6 +6,13 @@ def guild_creation(request):
 
     template_name = 'guild_creation/guild_creation.html'
 
+
     if request.method == 'GET':
         form = GuildForm()
         return render(request, template_name, {'form': form})
+
+    if request.method == 'POST':
+        form = GuildForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, template_name, {'form': form, 'text': text})
