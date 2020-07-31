bf69377ed3b0f7b9a677d495122faef73a0d2627
Maciej Król <maciej.krol16@interia.pl>
2020-07-31 13:15:59 +0200

    solve bug with crypto search and symbol handling

--- stock/crypto_stock/graphs.py
+++ stock/crypto_stock/graphs.py
@@ -36,8 +36,10 @@ def refresh_data(crypto_symbol, history_precision):
         elif history_precision == 'day':
             data = api_handler.get_daily_history(crypto_symbol, 'USD', 100)
     except KeyError:
-        # TODO handling KeyError from python form
-        return
+        crypto_symbol = "BTC"
+        data = api_handler.get_hourly_history(crypto_symbol, 'USD', 200)
+    return crypto_symbol
+
     x_data.clear()
     y_data_box_open.clear()
     y_data_scatter.clear()
@@ -57,7 +59,7 @@ def return_app(crypto_symbol=None):
     global app
     if(crypto_symbol == '' or crypto_symbol is None):
         crypto_symbol = 'BTC'
-    refresh_data(crypto_symbol, 'hour')
+    crypto_symbol = refresh_data(crypto_symbol, 'hour')
     app = DjangoDash('CryptoGraph')
 
     app.layout = html.Div(style={'backgroundColor': colors['background'], 'margin-left': '0px',
