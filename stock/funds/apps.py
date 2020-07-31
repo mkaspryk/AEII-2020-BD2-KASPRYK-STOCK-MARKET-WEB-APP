d3624ed4f92e3f7a9a2d6c076504bb53c8b1a694
mkaspryk <mkaspryk09@outlook.com>
2020-07-31 08:09:01 +0200

    Revert "Stock funds stuff."
    This reverts commit b4af849a8b0750e9178e4f063febeb8940910a87.

--- stock/funds/apps.py
+++ stock/funds/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class PortfolioConfig(AppConfig):
-    name = 'portfolio'
+class FundsConfig(AppConfig):
+    name = 'funds'
--- stock/funds/models.py
+++ stock/funds/models.py
@@ -4,14 +4,15 @@ from django.dispatch import receiver
 from currencies.models import Currency
 from decimal import Decimal, ROUND_HALF_UP
 
+
 @receiver(post_save, sender='portfolio.UserWallet')
 def add_initial_funds(sender, instance, created, **kwargs):
     if created:
-        StockFund.objects.create(owner=instance, currency=Currency.objects.get(name="bitcoin"), amount=1.00)
+        Fund.objects.create(owner=instance, currency=Currency.objects.get(name="bitcoin"), amount=1.00)
 
 
-class StockFund(models.Model):
-    name = models.CharField(max_length = 40)
+class Fund(models.Model):
+    owner = models.ForeignKey('portfolio.UserWallet', on_delete=models.DO_NOTHING)
     currency = models.ForeignKey('currencies.Currency', on_delete=models.DO_NOTHING)
     amount = models.DecimalField(max_digits=20, decimal_places=2)
 
@@ -24,22 +25,6 @@ class StockFund(models.Model):
     def get_dollar_value(self):
         unrounded = self.get_dollar_value_unrounded()
         return unrounded.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)  # returns rounded value intentionally
-
-    def add_fund_value(self, changed_amount):
-        amount = amount + changed_amount
-        
-
-
-class StockFundUser(models.Model):
-	stockfundname = models.ForeignKey('StockFund', on_delete=models.CASCADE)
-	isOwner = models.BooleanField(default=False)
-	name = models.CharField(max_length = 40)
-	wallet = models.ForeignKey('portfolio.UserWallet', on_delete=models.DO_NOTHING, default = "")
-	def add_wallet_value(self, changed_amount):
-		pay_fund = self.fund_set.get(currency=self.stockfundname.currency)
-		new_amount = pay_fund.amount + changed_amount
-		pay_fund.amount = new_amount
-		pay_fund.save()
 
 
 
